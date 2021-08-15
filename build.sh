bf694e47677858db70f6a5e9605280c4eeb6c14b
swz-gh <contact@swz.works>
2021-08-15 19:39:40 +0200

    added mkdir build to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # builds the app binary inside docker and copies it to ./build/codescuffle
+mkdir build
 docker build --tag codescuffle .
 docker create --rm --name scuffle_tmp codescuffle
 docker cp scuffle_tmp:/app/codescuffle ./build/codescuffle
