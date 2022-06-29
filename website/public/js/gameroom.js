b4e3a3f839db79934ef617ccb849fee05590e3dc
Andriamanitra <mikko77792@gmail.com>
2022-06-30 02:54:15 +0300

    indicate test success with green or red outline

--- website/public/js/gameroom.js
+++ website/public/js/gameroom.js
@@ -7,10 +7,13 @@ const socket = new WebSocket(WS_URL)
 let ownerStatus = false
 let roundInProgress = false
 let startButton = document.getElementById("start-button")
+let testButton = document.getElementById("test-button")
 let submitButton = document.getElementById("submit-button")
+let lastAttempt = { code: "", language: "" }
 
 startButton.addEventListener("click", startRound)
 submitButton.addEventListener("click", submitCode)
+testButton.addEventListener("click", testCode);
 
 function updateButtonVisibility() {
     if (roundInProgress) {
@@ -24,18 +27,19 @@ function updateButtonVisibility() {
     }
 }
 
+function getSubmission() {
+    const code = window.editor.getValue()
+    const language = document.getElementById("language-selector").value
+    return { code, language }
+}
+
 function submitCode() {
-    let code = window.editor.getValue();
-    let selectedLanguage = document.getElementById("language-selector").value;
-    if (code == "") {
+    const subm = getSubmission()
+    if (subm.code == "") {
         alert("Cannot submit empty code!")
         return
     }
-    let submission = {
-        language: selectedLanguage,
-        code: code
-    };
-    socket.send("SUBMIT:" + JSON.stringify(submission))
+    socket.send("SUBMIT:" + JSON.stringify(subm))
 }
 
 function startRound() {
@@ -55,6 +59,12 @@ function showTestCase(testcase) {
     let testcaseRoot = document.getElementById("test-cases")
     let parent = document.createElement("div")
     parent.classList.add("test-case")
+    parent.addEventListener("click", (ev) => {
+        for (const el of document.getElementsByClassName("selected-test")) {
+            el.classList.remove("selected-test")
+        }
+        parent.classList.add("selected-test")
+    })
     parent.appendChild(testInputEl)
     parent.appendChild(testOutputEl)
     testcaseRoot.appendChild(parent)
@@ -69,6 +79,45 @@ function showPuzzle(puzzle) {
     const testContainer = document.getElementById("test-cases")
     testContainer.innerHTML = ""
     puzzle.tests.forEach(showTestCase)
+    document.querySelector(".test-case").classList.toggle("selected-test")
+}
+
+
+function testCode(ev) {
+    const selectedTest = document.querySelector(".selected-test")
+    const testOutputEl = selectedTest.querySelector(".test-output")
+    const testInputEl = selectedTest.querySelector(".test-input")
+    const outputContainer = document.querySelector(".output-container")
+    const expected = testOutputEl.innerText.trimEnd()
+    const subm = getSubmission()
+    const inputString = testInputEl.innerText
+    if (subm.language !== lastAttempt.language || subm.code !== lastAttempt.code) {
+        for (const cls of ["failed-test", "successful-test"]) {
+            for (const el of [...document.getElementsByClassName(cls)]) {
+                el.classList.remove(cls)
+            }
+        }
+    }
+    lastAttempt = subm
+    executeCode(subm.code, subm.language, inputString)
+        .then((resp) => resp.json())
+        .then((data) => {
+            const stdout = data.run.stdout.trimEnd()
+            const stderr = data.run.stderr.trimEnd()
+            document.getElementById("stdout").innerText = stdout
+            document.getElementById("stderr").innerText = stderr
+            if (stdout === expected) {
+                selectedTest.classList.remove("failed-test")
+                outputContainer.classList.remove("failed-test")
+                selectedTest.classList.add("successful-test")
+                outputContainer.classList.add("successful-test")
+            } else {
+                selectedTest.classList.remove("successful-test")
+                outputContainer.classList.remove("successful-test")
+                selectedTest.classList.add("failed-test")
+                outputContainer.classList.add("failed-test")
+            }
+        })
 }
 
 socket.addEventListener("open", function (event) {
--- website/public/js/piston-client.js
+++ website/public/js/piston-client.js
@@ -4,8 +4,6 @@ const EXECUTE_API = "https://emkc.org/api/v2/piston/execute"
 let languages = []
 let preferredLanguage = localStorage.getItem("preferred_language") || "python"
 
-document.getElementById("test-button").addEventListener("click", testCode);
-
 window.addEventListener("load", fetchLanguages);
 
 function fetchLanguages() {
@@ -13,10 +11,10 @@ function fetchLanguages() {
     .then(resp => resp.json())
     .then(data => {
       languages = data
-      languages.sort((a,b) => a.value < b.value ? 1 : -1)
+      languages.sort((a, b) => a.value < b.value ? 1 : -1)
       const languageSelector = this.document.getElementById("language-selector");
       languageSelector.innerHTML = "";
-      for(const obj of languages) {
+      for (const obj of languages) {
         let option = document.createElement("option");
         option.text = obj.language[0].toUpperCase() + obj.language.substring(1);
         option.value = obj.language;
@@ -26,14 +24,9 @@ function fetchLanguages() {
     })
 }
 
-function testCode(ev) {
-  let code = window.editor.getValue();
-  let selectedLanguage = document.getElementById("language-selector").value;
+function executeCode(code, selectedLanguage, stdin) {
   let languageVersion = languages.find(x => x.language == selectedLanguage).version;
 
-  // TODO: get inputString from currently selected test case
-  let inputString = document.querySelector(".test-input")?.innerText || ""
-  
   let codeRunRequest = {
     language: selectedLanguage,
     version: languageVersion,
@@ -42,19 +35,13 @@ function testCode(ev) {
         content: code
       }
     ],
-    stdin: inputString
+    stdin: stdin
   };
-  fetch(EXECUTE_API, {
+  return fetch(EXECUTE_API, {
     method: "post",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(codeRunRequest),
   })
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data)
-      document.getElementById("stdout").innerText = data.run.stdout;
-      document.getElementById("stderr").innerText = data.run.stderr;
-    });
 }
